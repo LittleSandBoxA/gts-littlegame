67fb36cd9f0f5cae1c96ca46c1d831399a90945b
2439905184 <suchenglong018@126.com>
2022-08-15 20:18:11 +0800

    🦄 refactor: 将自照摄像头加回来了

--- scripts/LittleGame/Player.gd
+++ scripts/LittleGame/Player.gd
@@ -2,13 +2,13 @@ extends KinematicBody
 
 var anim
 var grabState=false
-var cam_back
-var cam
+onready var selfCamera = $selfCam
+onready var fpsCamera = $fps
+onready var thirdCamera = $thirdCam
 var root
 var weight_display
 var sound_level
 var Item
-var fps_cam
 var ItemStack_Pointer_index
 
 var size
@@ -26,28 +26,17 @@ func _ready():
 	if get_tree().current_scene.name == "AI_test":
 		connect("set_height",get_parent().get_node("score_root"),"_set_height")
 #	root=find_parent("root")
-#	player=get_node("miku")
-#
-#	fps_cam=root.get_node("KinematicBody/miku/fps")
 #	command_box=root.get_node("command_root")
-#	New_AI=root.get_node("newAI")
 #	Item=root.get_node("item_stack_root/item_Stack")
-##	ItemStack_Pointer_index=root.get_node("item_stack_root/pointer").pointer_index
+#	ItemStack_Pointer_index=root.get_node("item_stack_root/pointer").pointer_index
 #
-#	sound_level=root.get_node("Game_over_root/sound/level")
-#	#sound_level.play()
 #	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-#	anim=get_node("miku/AnimationPlayer")
-#	#移动速度
-#	#speed = 5
-#	cam_back=root.get_node("KinematicBody/miku/behand")
-#	cam=root.get_node("KinematicBody/miku/Camera2")
 #	score_display=root.get_node("score_root/score_display")
 #	weight_display=root.get_node("score_root/weight_display")
-	pass # Replace with function body.
+	pass
 func _physics_process(delta):
 	move_and_slide(vel *speed)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	vel.z = Input.get_action_strength("walk") - Input.get_action_strength("walk_back")
 	#print_debug(vel)
@@ -63,14 +52,15 @@ func _process(delta):
 #		rotate_y(2*delta)
 #	if Input.is_action_pressed("rotate_right"):
 #		rotate_y(-2*delta)
+	if vel.z == 1 or vel.z == -1:
+		#print_debug("走路")
+		$rhea/AnimationPlayer.play("女性走路气质001")
 	if (vel.z == 1 or vel.z == -1) and Input.is_action_pressed("run_faster"):
-		print_debug("加速跑")
+		#print_debug("加速跑")
 		speed = 4
-		$rhea/rhea/AnimationPlayer.play("有力的女性跑步")
-#	if Input.is_action_pressed("look_behind"):
-#		cam_back.make_current()
-#	if Input.is_action_just_released("look_behind"):
-#		cam.make_current()
+		$rhea/AnimationPlayer.play("有力的女性跑步")
+	if Input.is_action_pressed("look_behind"):
+		selfCamera.make_current()
 	#weight_display.text=str("体重"+str(anim.weight))
 	#变小
 	if Input.is_action_pressed("z"):
@@ -91,9 +81,10 @@ func _input(event):
 	#这个是grab
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == BUTTON_LEFT:
 		pass
-	if event.is_action_pressed("walk"):
-		$rhea/rhea/AnimationPlayer.play("女性走路气质001")
 	if event.is_action_released("walk"):
-		$rhea/rhea/AnimationPlayer.stop()
+		$rhea/AnimationPlayer.stop()
 		speed = 2
+	if event.is_action_released("look_behind"):
+		thirdCamera.make_current()
+		pass
 	pass
