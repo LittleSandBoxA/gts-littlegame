f73920f305ee065fc413250be6a5c7775993ac5a
2439905184 <suchenglong018@126.com>
2022-08-15 19:20:39 +0800

    🎈 perf: 优化了一下菜单界面
    AI_Test场景

---
+++ scenes/layout/game_over.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+func _on_Resume_pressed():
+	self.hide()
+	pass
+
+func _on_Restart_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+	pass
+
+func _on_MainMenu_pressed():
+	get_tree().paused = false
+	get_tree().change_scene("res://scenes/layout/Title.tscn")
+	pass
+
+
+func _on_Game_over_root_visibility_changed():
+	if self.visible:
+		get_tree().paused = true
+	else:
+		get_tree().paused = false
+	pass
--- scripts/ccontrol.gd
+++ scripts/ccontrol.gd
@@ -21,3 +21,8 @@ func _input(event):
 	if event.is_action_pressed("backToMenu"):
 		Game_Over.show()
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func _on_Menu_pressed():
+	$Game_over_root.show()
+	pass
