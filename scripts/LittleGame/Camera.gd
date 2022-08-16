a949967732463bd1dea82f1b11c27af37dacf433
2439905184 <suchenglong018@126.com>
2022-08-16 23:30:42 +0800

    ✨ feat: 添加了第三人称控制
    走路+跑步

---
+++ scripts/LittleGame/Camera.gd
@@ -0,0 +1,35 @@
+extends Spatial
+
+#此脚本用于3D模式的第三人称摄像机
+
+var cam_rot_h = 0
+var cam_rot_v = 0
+#垂直范围限制
+var cam_v_min = -55
+var cam_v_max = 75
+#灵敏度
+var h_sense = 0.1
+var v_sense = 0.1
+#加速度
+var h_accel = 10
+var v_accel = 10
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	$h/v/thirdCam.add_exception(get_parent())
+	pass
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		cam_rot_h += -event.relative.x
+		cam_rot_v += event.relative.y
+		
+	pass
+
+func _physics_process(delta):
+	cam_rot_v = clamp(cam_rot_v, cam_v_min, cam_v_max)
+	$h.rotation_degrees.y = lerp($h.rotation_degrees.y, cam_rot_h, delta * h_accel)
+	$h/v.rotation_degrees.x = lerp($h/v.rotation_degrees.x, cam_rot_v, delta * v_accel)
+	
+	
+	pass
--- scripts/LittleGame/Player.gd
+++ scripts/LittleGame/Player.gd
@@ -8,90 +8,94 @@ onready var thirdCamera = $thirdCam
 var root
 var weight_display
 var sound_level
-var Item
-var ItemStack_Pointer_index
-
 var size
 
-export var speed = 2 #变大后的移动速度
-
+#当前速度
+var speed = 0
+var walk_speed = 2
+var run_speed = 5
 var player
 var distance_to_gts
 var command_box
 var height = 1.5
 onready var tween = $Tween
 
-signal set_height
+
 var vel = Vector3(0,0,0)
+var direction = Vector3(0,0,0)
+#加速度
+var accel = 6
+#纵向和横向速度
+var vertical_vel = 0
+var gravity = 20
+var angular_accel = 7
+signal set_height
 
 func _ready():
 	if get_tree().current_scene.name == "AITest":
 		var err1 = connect("set_height",get_parent().get_node("ScoreRoot"),"_set_height")
 		if err1 != OK:
 			print_debug("链接set_height信号出错了",err1)
-		
-#	command_box=root.get_node("command_root")
-#	Item=root.get_node("item_stack_root/item_Stack")
-#	ItemStack_Pointer_index=root.get_node("item_stack_root/pointer").pointer_index
-#
-#	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-#	score_display=root.get_node("score_root/score_display")
-#	weight_display=root.get_node("score_root/weight_display")
 	pass
 # warning-ignore:unused_argument
 func _physics_process(delta):
-# warning-ignore:return_value_discarded
-	move_and_slide(vel *speed)
-
-# warning-ignore:unused_argument
-func _process(delta):
-	vel.z = Input.get_action_strength("walk") - Input.get_action_strength("walk_back")
-	#print_debug(vel)
-	#移动速度
-	#speed=anim.moveSpeed
-	#print("实时速度"+str(speed*delta))
-	#distance_to_gts=self.translation.distance_to(New_AI.translation)
-	#size=player.get_scale()
-	#bug修复 应该使用move and slide
-#	if Input.is_action_pressed("walk_back"):
-#		move_and_slide(Vector3(0,0,-speed))
-#	if Input.is_action_pressed("rotate_left"):
-#		rotate_y(2*delta)
-#	if Input.is_action_pressed("rotate_right"):
-#		rotate_y(-2*delta)
-	if vel.z == 1 or vel.z == -1:
-		#print_debug("走路")
-		$rhea/AnimationPlayer.play("女性走路气质001")
-	if (vel.z == 1 or vel.z == -1) and Input.is_action_pressed("run_faster"):
-		#print_debug("加速跑")
-		speed = 4
-		$rhea/AnimationPlayer.play("有力的女性跑步")
-	if Input.is_action_pressed("look_behind"):
-		selfCamera.make_current()
-	#weight_display.text=str("体重"+str(anim.weight))
-	#变小
-	if Input.is_action_pressed("z"):
-		self.scale *= 0.99
-		if height > 0:
-			height -= 0.9
-			speed -= 1.05
-			emit_signal("set_height",Utils.humanize_size(height))
-	#变大
-	if Input.is_action_pressed("x"):
-		self.scale *= 1.01
-		speed += 1.05
-		height = 1.5*self.scale.x
-		emit_signal("set_height",Utils.humanize_size(height))
-	pass
-
-func _input(event):
-	#这个是grab
-	if event is InputEventMouseButton and event.is_pressed() and event.button_index == BUTTON_LEFT:
-		pass
-	if event.is_action_released("walk"):
+	# warning-ignore:return_value_discarded
+	#move_and_slide(vel *speed)
+	if Input.is_action_pressed("W") or Input.is_action_pressed("S") or Input.is_action_pressed("A") or Input.is_action_pressed("D"):
+		var h_rot = $CameraRoot/h.global_transform.basis.get_euler().y
+		direction = Vector3(0,0,Input.get_action_strength("W") - Input.get_action_strength("S")).rotated(Vector3.UP, h_rot).normalized()
+		if Input.is_action_pressed("run_faster"):
+			speed = run_speed
+			$rhea/AnimationPlayer.play("有力的女性跑步")
+		else:
+			speed = walk_speed
+			$rhea/AnimationPlayer.play("女性走路气质001")
+	else:
+		speed = 0
 		$rhea/AnimationPlayer.stop()
-		speed = 2
-	if event.is_action_released("look_behind"):
-		thirdCamera.make_current()
-		pass
+		
+	vel = lerp(vel, direction * speed , delta * accel)
+	move_and_slide(vel + Vector3.DOWN * vertical_vel, Vector3.UP)
+	if not is_on_floor():
+		vertical_vel += gravity * delta
+	else:
+		vertical_vel = 0
+	var mesh = $"rhea/レアV1 12/レアV1 12_arm/Skeleton/レアV1 12_mesh"
+	mesh.rotation.y = lerp_angle(mesh.rotation.y, atan2(direction.x, direction.z), delta * angular_accel)
 	pass
+# warning-ignore:unused_argument
+#func _process(delta):
+#	#vel.z = Input.get_action_strength("walk") - Input.get_action_strength("walk_back")
+#	#变小
+#	if Input.is_action_pressed("z"):
+#		self.scale *= 0.99
+#		if height > 0:
+#			height -= 0.9
+#			speed -= 1.05
+#			emit_signal("set_height",Utils.humanize_size(height))
+#	#变大
+#	if Input.is_action_pressed("x"):
+#		self.scale *= 1.01
+#		speed += 1.05
+#		height = 1.5*self.scale.x
+#		emit_signal("set_height",Utils.humanize_size(height))
+#	pass
+
+#func _input(event):
+#	#测试用
+##	if event is InputEventKey:
+##		if event.as_text() == "W" or event.as_text() == "A" or event.as_text() == "S" or event.as_text() == "D" or event.as_text() == "Space":
+##			if event.pressed:
+##				pass
+##			else:
+##				pass
+#	#这个是grab
+#	#if event is InputEventMouseButton and event.is_pressed() and event.button_index == BUTTON_LEFT:
+##		pass
+##	if event.is_action_released("walk"):
+##		$rhea/AnimationPlayer.stop()
+##		speed = 2
+##	if event.is_action_released("look_behind"):
+##		thirdCamera.make_current()
+##		pass
+#	pass
