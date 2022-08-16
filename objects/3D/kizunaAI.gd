ee768bd5501036a39e2d99964314db8c7634aac7
2439905184 <suchenglong018@126.com>
2022-08-16 17:37:29 +0800

    ✨ feat: 新增角色 绊爱

---
+++ objects/3D/kizunaAI.gd
@@ -0,0 +1,14 @@
+tool
+extends Spatial
+
+var height = 1.56 #单位米
+var weight = 46 #单位kg
+
+func _ready():
+	$AnimationPlayer.get_animation("女性走路气质001").loop = true
+	$AnimationPlayer.play("女性走路气质001")
+	pass
+
+func _process(delta):
+	rotate_y(2*delta)
+	pass
