5c58bdd78664c883c11b687b1e17e9f93358bc5b
2439905184 <suchenglong018@126.com>
2022-08-21 16:02:07 +0800

    🐞 fix: 初步完善放置功能
    但是有重叠问题

--- scripts/LittleGame/RayCastCompent.gd
+++ scripts/LittleGame/RayCastCompent.gd
@@ -1,10 +1,13 @@
 extends RayCast
 
 onready var piano = load("res://objects/3D/Piano.tscn")
-onready var pianoInstance = piano.instance()
+
+var placed = false
+
 func _process(delta):
 	if is_colliding():
-		if get_collider():
+		if get_collider() and Input.is_mouse_button_pressed(BUTTON_RIGHT) and not placed:
+			var pianoInstance = piano.instance()
 			pianoInstance.translation = get_collision_point()
 			get_tree().current_scene.add_child(pianoInstance)
 		#pianoInstance.translation = cast_to
